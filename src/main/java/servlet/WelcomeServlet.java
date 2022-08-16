d7ef6892b78002f0013e187dfe055980ddcabc6b
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-16 10:45:28 +0800

    doGet(), doPost(), doPut(), doDelete()

--- src/main/java/servlet/WelcomeServlet.java
+++ src/main/java/servlet/WelcomeServlet.java
@@ -13,19 +13,31 @@ import javax.servlet.http.HttpServletResponse;
 @WebServlet("/servlet/welcome")
 public class WelcomeServlet extends HttpServlet {
 	
-	// 通常用於網址列的請求
+	// 通常用於網址列的請求(查詢資料)
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
 		out.println("Welcome doGet()... " + new Date());
-		
 	}
 	
-	// 通常用於表單的請求
+	// 通常用於表單的請求(新增資料)
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		PrintWriter out = resp.getWriter();
 		out.println("Welcome doPost()... " + new Date());
 	}
 	
+	// 通常用於表單的請求(修改資料)
+	@Override
+	protected void doPut(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Welcome doPut()... " + new Date());
+	}
+
+	// 通常用於表單的請求(刪除資料)
+	@Override
+	protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Welcome doDelete()... " + new Date());
+	}	
 }
