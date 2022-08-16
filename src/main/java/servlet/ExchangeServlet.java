d449f7ab99fdf965827555aa81342e7f65648db9
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-16 15:54:05 +0800

    exchange

---
+++ src/main/java/servlet/ExchangeServlet.java
@@ -0,0 +1,38 @@
+package servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/exchange")
+public class ExchangeServlet extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int amount = Integer.parseInt(req.getParameter("amount"));
+		String currency = req.getParameter("currency");
+		
+		// 得到匯率
+		String symbol = "TWD" + currency + "=x"; // 例如: TWDUSD=x, TWDJPY=x ...
+		double rate = 0.033;
+		
+		// 計算結果
+		double result = amount * rate;
+		
+		// 透過分派器將資料傳遞給 /form/exchange_form.jsp
+		// 1. 建立分派器
+		RequestDispatcher rd = req.getRequestDispatcher("/form/exchange_form.jsp");
+		// 2. 新增要傳遞的參數
+		req.setAttribute("amount", amount);
+		req.setAttribute("currency", currency);
+		req.setAttribute("result", result);
+		// 3. 重導到指定頁面
+		rd.forward(req, resp);
+	}
+	
+}
