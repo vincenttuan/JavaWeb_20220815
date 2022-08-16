11954d38035e38483d0953bc86e19fcd3a1a5ca3
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-16 10:33:19 +0800

    w

---
+++ src/main/java/servlet/WelcomeServlet.java
@@ -0,0 +1,30 @@
+package servlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.Date;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@WebServlet("/servlet/welcome")
+public class WelcomeServlet extends HttpServlet {
+	
+	// 通常用於網址列的請求
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Welcome doGet()... " + new Date());
+	}
+	
+	// 通常用於表單的請求
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter out = resp.getWriter();
+		out.println("Welcome doPost()... " + new Date());
+	}
+	
+}
