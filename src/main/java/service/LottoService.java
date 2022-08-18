128bc0cda2b92db963e2382b021de13345bb6d9d
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-18 10:03:47 +0800

    lotto

--- src/main/java/service/LottoService.java
+++ src/main/java/service/LottoService.java
@@ -1,5 +1,19 @@
 package service;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
 public class LottoService {
+	
+	// 四星採電腦選號
+	public List<Integer> getLotto() {
+		Random random = new Random();
+		List<Integer> nums = new ArrayList<>();
+		for(int i=0;i<4;i++) {
+			nums.add(random.nextInt(10));  // 0~9
+		}
+		return nums;
+	}
 
 }
