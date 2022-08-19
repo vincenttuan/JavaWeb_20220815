f2470894adf6e6da7d8720b0d4ad43ea553fe1cd
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-19 11:45:25 +0800

    web

--- src/main/java/mvc/service/EncryptDESService.java
+++ src/main/java/mvc/service/EncryptDESService.java
@@ -23,9 +23,9 @@ public class EncryptDESService {
 	// KeyGenerator 提供對稱密鑰生成器的功能，支持各種演算法
     private KeyGenerator keygen;
     // SecretKey 負責保存對稱密鑰
-    private SecretKey deskey;
+    private SecretKey secretkey;
     // Cipher 負責完成加密或解密工作
-    private Cipher c;
+    private Cipher cipher;
     // 該字節數組負責保存加密的結果
     private byte[] cipherByte;
     
@@ -33,35 +33,37 @@ public class EncryptDESService {
 	public void genKey(String path) throws Exception {
 		// 實例化支持 DES 演算法的密鑰生成器(演算法名稱命名需按規定，否則拋出異常)
 		KeyGenerator keygen = KeyGenerator.getInstance("DES");
-		File file = new File(path);
 		
+		File file = new File(path);
 		if(file.exists()){
             ObjectInputStream ois = new ObjectInputStream(new FileInputStream(file));
-            deskey = (SecretKey)ois.readObject();
+            secretkey = (SecretKey)ois.readObject();
         } else {
             // 生成密鑰
-            deskey = keygen.generateKey();
+        	secretkey = keygen.generateKey();
             ObjectOutputStream oos = new ObjectOutputStream(new FileOutputStream(file));
-            oos.writeObject(deskey);
+            oos.writeObject(secretkey);
         }
         
 		// 生成 Cipher 物件,指定其支持的 DES 演算法
-        c = Cipher.getInstance("DES"); // Cipher 負責完成加密或解密工作
+		cipher = Cipher.getInstance("DES"); // Cipher 負責完成加密或解密工作
 	}
 	
+	// 加密方法
 	public byte[] encrytor(String str) throws Exception {
         // 根據密鑰，對 Cipher 物件進行初始化，ENCRYPT_MODE 表示加密模式
-        c.init(Cipher.ENCRYPT_MODE, deskey);
+		cipher.init(Cipher.ENCRYPT_MODE, secretkey);
         byte[] src = str.getBytes();
         // 加密，結果保存進 cipherByte
-        byte[] cipherByte = c.doFinal(src);
+        byte[] cipherByte = cipher.doFinal(src);
         return cipherByte;
     }
 	
+	// 解密方法
 	public byte[] decryptor(byte[] buff) throws Exception {
         // 根據密鑰，對 Cipher 物件進行初始化，DECRYPT_MODE 表示加密模式
-        c.init(Cipher.DECRYPT_MODE, deskey);
-        cipherByte = c.doFinal(buff);
+		cipher.init(Cipher.DECRYPT_MODE, secretkey);
+        cipherByte = cipher.doFinal(buff);
         return cipherByte;
     }
 	
