094927a245e9ffeb82ab84587d6c4f3ea607891b
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-19 11:31:30 +0800

    web

--- src/test/java/security/EncrypDESServiceTest.java
+++ src/test/java/security/EncrypDESServiceTest.java
@@ -7,16 +7,21 @@ import mvc.service.EncryptDESService;
 
 public class EncrypDESServiceTest {
 	public static void main(String[] args) throws Exception {
-		String path = "src/main/java/mvc/key/user.key";
-		EncryptDESService de1 = new EncryptDESService();
-		de1.genKey(path);
-
+		String path = "src/main/java/mvc/key/user.key"; // 密鑰位置
+		EncryptDESService de1 = new EncryptDESService(); // 建立 DES 加密服務
+		de1.genKey(path); // 產生或使用密鑰
+		
+		// 原文
 		String msg = "巨匠電腦 Java 課程";
-		byte[] encontent = de1.encrytor(msg); // 加密
-		byte[] decontent = de1.decryptor(encontent); // 解密
 		System.out.println("明文是:" + msg);
+		
+		// 加密
+		byte[] encontent = de1.encrytor(msg); // 加密
 		System.out.println("加密後:" + Arrays.toString(encontent));
 		System.out.println("加密後(16進位):" + String.format("%X", new BigInteger(encontent)));
+		
+		// 解密
+		byte[] decontent = de1.decryptor(encontent); // 解密
 		System.out.println("解密後:" + new String(decontent));
 	}
 }
