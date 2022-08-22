e2745d0efdef7fa5b9118702abf007081041c9c4
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-22 15:36:17 +0800

    web

--- src/main/java/mvc/filter/LoginFilter.java
+++ src/main/java/mvc/filter/LoginFilter.java
@@ -9,6 +9,10 @@ import javax.servlet.annotation.WebFilter;
 import javax.servlet.http.HttpFilter;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import mvc.entity.User;
+import mvc.service.UserService;
 
 // 登入驗證與管理
 @WebFilter(urlPatterns = {"/mvc/user/*"})
@@ -18,10 +22,29 @@ public class LoginFilter extends HttpFilter {
 	protected void doFilter(HttpServletRequest req, HttpServletResponse res, FilterChain chain)
 			throws IOException, ServletException {
 		
-		res.getWriter().print("Please login !!");
-		// 重導到 login 畫面
-		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
-		rd.forward(req, res);
+		UserService userService = new UserService();
+		
+		// 確認使用者是否已登入?
+		// 利用 session 變數來存放特定資料
+		HttpSession session = req.getSession();
+		// 查找是否在 session 變數中有 user 這個參數?
+		if(session.getAttribute("user") != null) {
+			chain.doFilter(req, res); // 放行
+		} else {
+			// 檢查是否有從表單傳來 username 與 password
+			String username = req.getParameter("username");
+			String password = req.getParameter("password");
+			User user = userService.checkLogin(username, password);
+			if(user != null) { // 是否登入成功
+				session.setAttribute("user", user); // 將 user(登入者) 物件存放在 session 變數(每一頁(servlet, jsp)都可以使用)中
+				chain.doFilter(req, res); // 放行
+			} else {
+				// 重導到 login 畫面
+				RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/login.jsp");
+				rd.forward(req, res);
+			}
+		}
+		
 	}
 	
 }
