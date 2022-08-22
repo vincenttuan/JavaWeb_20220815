76f52de13cd0d213f74d77388d40e32b3b89211b
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-22 09:57:22 +0800

    web

--- src/main/java/mvc/controller/UserController.java
+++ src/main/java/mvc/controller/UserController.java
@@ -10,17 +10,46 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import mvc.service.UserService;
+/*
+ * 設計路徑:
+ * Read   Get  /mvc/user/              全部查詢(首頁)
+ * Read   Get  /mvc/user/2             單筆查詢(查詢id=2的資料)
+ * Delete Get  /mvc/user/5?mode=delete 單筆刪除(刪除id=5的資料)
+ * Create Post /mvc/user/              新增
+ * Update Post /mvc/user/3             修改(修改id=3的資料)
+ * */
 
-@WebServlet("/mvc/user")
+@WebServlet("/mvc/user/*")
 public class UserController extends HttpServlet {
 	
 	private UserService userService = new UserService();
 	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		String pathInfo = req.getPathInfo();
+		System.out.println("pathInfo: " + pathInfo);
+		
+		if(pathInfo.equals("/")) { // 全部查詢
+			System.out.println("全部查詢模式");
+			
+		} else { // 單筆查詢
+			// 將 / 去除後轉 int (Ex: /2 變成 2)
+			Integer id = Integer.parseInt(pathInfo.replace("/", ""));
+			System.out.print("id: " + id + ", ");
+			// 判斷是否有 mode 參數傳入
+			String mode = req.getParameter("mode");
+			if(mode != null & mode.equals("delete")) {
+				System.out.println("刪除模式");
+				
+			} else {
+				System.out.println("單筆查詢模式");
+				
+			}
+		}
 		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/user.jsp");
 		req.setAttribute("users", userService.queryAll());
 		rd.forward(req, resp);
+		
 	}
 
 	@Override
--- src/main/webapp/index.jsp
+++ src/main/webapp/index.jsp
@@ -19,10 +19,10 @@
 		<li><a href="${ pageContext.request.contextPath }/form/exchange_form.jsp">exchange_form.jsp</a></li>
 		<li><a href="${ pageContext.request.contextPath }/servlet/lotto">/servlet/lotto (MVC 版)</a></li>
 		<hr />
-		<li><a href="${ pageContext.request.contextPath }/mvc/user">/mvc/user (JDBC)</a></li>
-		<li><a href="${ pageContext.request.contextPath }/mvc/product">/mvc/product (REST/JDBC)</a></li>
-		<li><a href="${ pageContext.request.contextPath }/mvc/cart">/mvc/cart (REST/JPA)</a></li>
-		<li><a href="${ pageContext.request.contextPath }/mvc/chat">/mvc/chat (WebSocket)</a></li>
+		<li><a href="${ pageContext.request.contextPath }/mvc/user/">/mvc/user/ (JDBC)</a></li>
+		<li><a href="${ pageContext.request.contextPath }/mvc/product/">/mvc/product/ (REST/JDBC)</a></li>
+		<li><a href="${ pageContext.request.contextPath }/mvc/cart/">/mvc/cart/ (REST/JPA)</a></li>
+		<li><a href="${ pageContext.request.contextPath }/mvc/chat/">/mvc/chat/ (WebSocket)</a></li>
 	</ul>
 </body>
 </html>
