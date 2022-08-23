5cae8e18ad8ad4627e671d1ea155f75e42f7bd8c
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-23 10:23:50 +0800

    web

--- src/main/java/mvc/controller/BookController.java
+++ src/main/java/mvc/controller/BookController.java
@@ -10,10 +10,24 @@ import javax.servlet.http.HttpServletResponse;
 
 @WebServlet("/mvc/book/*")
 public class BookController extends HttpServlet {
-
+	
+	private String getPathVariable(HttpServletRequest req) {
+		// 取得 path info 資料
+		String pathInfo = req.getPathInfo();
+		// 去除 path info 的前導 "/" 變為 path variable
+		String pathVariable = pathInfo.replace("/", "");
+		return pathVariable;
+	}
+	
+	/*
+	 * 查詢全部: /mvc/book/ 或 /mvc/book/*
+	 * 查詢單筆: /mvc/book/1
+	 * */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("do get 查詢資料用");
+		resp.getWriter().println("do get 查詢資料用 ");
+		String pathVar = getPathVariable(req);
+		resp.getWriter().println("pathVar: " + pathVar);
 	}
 
 	@Override
