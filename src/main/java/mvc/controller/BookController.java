ca6f81bc6f3fe485e3b7299ffd23640ad72d8ce7
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-23 10:34:20 +0800

    web

--- src/main/java/mvc/controller/BookController.java
+++ src/main/java/mvc/controller/BookController.java
@@ -16,6 +16,8 @@ public class BookController extends HttpServlet {
 		String pathInfo = req.getPathInfo();
 		// 去除 path info 的前導 "/" 變為 path variable
 		String pathVariable = pathInfo.replace("/", "");
+		// 去除 * 號
+		pathVariable = pathVariable.replace("*", "");
 		return pathVariable;
 	}
 	
@@ -25,14 +27,32 @@ public class BookController extends HttpServlet {
 	 * */
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().println("do get 查詢資料用 ");
 		String pathVar = getPathVariable(req);
-		resp.getWriter().println("pathVar: " + pathVar);
+		resp.getWriter().println("pathVar: " + pathVar + "<br />");
+		if(pathVar.length() == 0) {
+			resp.getWriter().println("doGet() 全部查詢資料用 <br />");
+		} else {
+			Integer id = Integer.parseInt(pathVar);
+			resp.getWriter().println("doGet() 單筆查詢資料用, id = " + id  + "<br />");
+		}
 	}
-
+	
+	/*
+	 * 新增: /mvc/book/
+	 * 資料由表單帶入
+	 * */
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		resp.getWriter().print("do post 新增資料用");
+		// 取得表單資料
+		String id = req.getParameter("id");
+		String name = req.getParameter("name");
+		String amount = req.getParameter("amount");
+		String price = req.getParameter("price");
+		resp.getWriter().print("doPost() 新增資料用 <br />");
+		resp.getWriter().print("id: " +  id + "<br />");
+		resp.getWriter().print("name: " +  name + "<br />");
+		resp.getWriter().print("amount: " +  amount + "<br />");
+		resp.getWriter().print("price: " +  price + "<br />");
 	}
 
 	@Override
