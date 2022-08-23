e8098e03df35adadd841c090cd07579bf6defbf2
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-23 13:21:31 +0800

    web

---
+++ src/main/java/mvc/entity/Book.java
@@ -0,0 +1,47 @@
+package mvc.entity;
+
+public class Book {
+	private Integer id;
+	private String name;
+	private Integer amount;
+	private Integer price;
+	private Integer userId;
+	
+	public Integer getId() {
+		return id;
+	}
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public Integer getAmount() {
+		return amount;
+	}
+	public void setAmount(Integer amount) {
+		this.amount = amount;
+	}
+	public Integer getPrice() {
+		return price;
+	}
+	public void setPrice(Integer price) {
+		this.price = price;
+	}
+	public Integer getUserId() {
+		return userId;
+	}
+	public void setUserId(Integer userId) {
+		this.userId = userId;
+	}
+	@Override
+	public String toString() {
+		return "Book [id=" + id + ", name=" + name + ", amount=" + amount + ", price=" + price + ", userId=" + userId
+				+ "]";
+	}
+	
+	
+}
