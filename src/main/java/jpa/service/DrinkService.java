e082a78fa24b025cef17347e1000f61d13c469ba
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-25 11:53:49 +0800

    web

--- src/main/java/jpa/service/DrinkService.java
+++ src/main/java/jpa/service/DrinkService.java
@@ -28,6 +28,38 @@ public class DrinkService {
 		return isSuccess;
 	}
 	
+	// 修改
+	public boolean modify(String id, String name, String amount, String price) {
+		// 資料驗證
+		if(id == null || name == null || name.length() == 0 || 
+			amount == null || price == null || 
+			!id.chars().allMatch(Character::isDigit) || 
+			!amount.chars().allMatch(Character::isDigit) || 
+			!price.chars().allMatch(Character::isDigit)) {
+			return false;
+		}
+		// 建立 drink 物件
+		Drink drink = new Drink();
+		drink.setId(Integer.parseInt(id)); // 需要放入 id
+		drink.setName(name);
+		drink.setAmount(Integer.parseInt(amount));
+		drink.setPrice(Integer.parseInt(price));
+		// 進行修改 (isSuccess 是否成功 ?)
+		boolean isSuccess = drinkDao.update(drink);
+		return isSuccess;
+	}
+	
+	// 刪除
+	public boolean remove(String id) {
+		// 資料驗證
+		if(id == null || !id.chars().allMatch(Character::isDigit)) {
+			return false;
+		}
+		// 進行移除 (isSuccess 是否成功 ?)
+		boolean isSuccess = drinkDao.delete(Integer.parseInt(id));
+		return isSuccess;
+	}
+	
 	// 查詢全部
 	public List<Drink> findAll() {
 		return drinkDao.query();
