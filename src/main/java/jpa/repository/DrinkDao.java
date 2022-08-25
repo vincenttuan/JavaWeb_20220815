abf35a719bcdcbdde8693b1d0fd93e5f6d28a707
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-25 11:48:42 +0800

    web

--- src/main/java/jpa/repository/DrinkDao.java
+++ src/main/java/jpa/repository/DrinkDao.java
@@ -31,6 +31,40 @@ public class DrinkDao {
 		return true;
 	}
 	
+	// 修改
+	public synchronized boolean update(Drink drink) {
+		// 先判斷有無此筆資料
+		if(get(drink.getId()) == null) {
+			return false;
+		} else {
+			EntityTransaction etx = em.getTransaction(); // 取得交易環境進行新增,修改,刪除工作
+			etx.begin(); // 交易開始
+			em.merge(drink); // 將 drink 物件進行永續(將 drink 存放到資料表的紀錄中)
+			etx.commit(); // 提交(交易結束)
+			return true;
+		}
+	}
+	
+	// 刪除
+	public synchronized boolean delete(Integer id) {
+		// 先判斷有無此筆資料
+		Drink drink = get(id);
+		if(drink == null) {
+			return false;
+		} else {
+			EntityTransaction etx = em.getTransaction(); // 取得交易環境進行新增,修改,刪除工作
+			etx.begin(); // 交易開始
+			em.remove(drink); // 將 drink 紀錄移除
+			etx.commit(); // 提交(交易結束)
+			return true;
+		}
+	}
+	
+	// 單筆查詢
+	public Drink get(Integer id) {
+		return em.find(Drink.class, id);
+	}
+	
 	// 全部查詢
 	public List<Drink> query() {
 		//Query query = em.createQuery("from Drink d", Drink.class); // JPQL
