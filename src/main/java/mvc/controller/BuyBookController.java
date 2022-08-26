27c79e86af7411d9488630e3686aad18db84e1c0
MB-teacher <MB-teacher@MB-207-teacher>
2022-08-26 10:19:38 +0800

    web

--- src/main/java/mvc/controller/BuyBookController.java
+++ src/main/java/mvc/controller/BuyBookController.java
@@ -2,18 +2,26 @@ package mvc.controller;
 
 import java.io.IOException;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import mvc.service.BookService;
+
 @WebServlet("/mvc/buy/book/")
 public class BuyBookController extends HttpServlet {
-
+	
+	private BookService bookService = new BookService();
+	
 	@Override
 	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		
+		// 重導到指定頁面
+		RequestDispatcher rd = req.getRequestDispatcher("/WEB-INF/view/buy_book.jsp");
+		req.setAttribute("books", bookService.queryAll());
+		rd.forward(req, resp);
 	}
 
 	@Override
